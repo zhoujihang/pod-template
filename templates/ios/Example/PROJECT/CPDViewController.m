c09b051f5da1fe0cb2ae5ec672fac033e7a1d04a
zhoujihang <zhoujihang@bytedance.com>
2019-09-23 14:27:19 +0800

    调试

--- templates/ios/Example/PROJECT/CPDViewController.m
+++ templates/ios/Example/PROJECT/CPDViewController.m
@@ -7,23 +7,107 @@
 //
 
 #import "CPDViewController.h"
+#import <Masonry/Masonry.h>
 
-@interface CPDViewController ()
+@interface CPDViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong, nullable) UITableView *tableView;
 
 @end
 
 @implementation CPDViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self setup];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setup {
+    [self setupView];
+    [self setupFrame];
+    [self setupData];
+}
+
+- (void)setupView {
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.alwaysBounceVertical = YES;
+    self.tableView.estimatedRowHeight = 0;
+    self.tableView.estimatedSectionHeaderHeight = 0;
+    self.tableView.estimatedSectionFooterHeight = 0;
+    self.tableView.backgroundColor = [UIColor whiteColor];
+    self.tableView.showsVerticalScrollIndicator = NO;
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    
+    [self.view addSubview:self.tableView];
+}
+
+- (void)setupFrame {
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view);
+    }];
+}
+
+- (void)setupData {
+    
+}
+
+#pragma mark - UITableView 代理方法
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    NSString *text = [[NSString alloc] initWithFormat:@"%ld - %ld", indexPath.section, indexPath.row];
+    if (indexPath.row == 0) {
+        text = @"测试 1";
+    } else if (indexPath.row == 1) {
+        text = @"测试 2";
+    } else if (indexPath.row == 2) {
+        text = @"测试 3";
+    }
+    cell.textLabel.text = text;
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 44;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    return nil;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 0.01;
+}
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
+    return nil;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    return 0.01;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    SEL sel = NSSelectorFromString([NSString stringWithFormat:@"func%ld", indexPath.row]);
+    if ([self respondsToSelector:sel]) {
+        [self performSelector:sel];
+    }
+}
+
+- (void)func0 {
+}
+
+- (void)func1 {
+}
+
+- (void)func2 {
 }
 
 @end
